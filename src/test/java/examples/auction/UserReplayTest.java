7b83b8cbd7f340d5dddb8d7b898805222f8f7187
alex <rojkov@gmail.com>
2015-05-04 17:13:29 -0700

    auction replay test

---
+++ src/test/java/examples/auction/UserReplayTest.java
@@ -0,0 +1,59 @@
+package examples.auction;
+
+import com.caucho.junit.ConfigurationBaratine;
+import com.caucho.junit.RunnerBaratine;
+import io.baratine.core.Lookup;
+import io.baratine.core.ServiceRef;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.inject.Inject;
+
+/**
+ * Unit test for user create with Journal.
+ */
+@RunWith(RunnerBaratine.class)
+@ConfigurationBaratine(services = {IdentityManagerImpl.class, UserManagerImpl.class}, pod = "user",
+  logLevel = "FINER",
+  logs = {@ConfigurationBaratine.Log(name = "com.caucho", level = "FINER"),
+          @ConfigurationBaratine.Log(name = "examples.auction", level = "FINER")},
+  port = 6810)
+public class UserReplayTest
+{
+  @Inject @Lookup("pod://user/user")
+  UserManagerSync _userManager;
+
+  @Inject @Lookup("pod://user/user")
+  ServiceRef _userManagerRef;
+
+  @Inject
+  RunnerBaratine _testContext;
+
+  /**
+   * User create correctly sets the user name.
+   */
+  @Test
+  public void createUser()
+  {
+    final String id = _userManager.create("Spock", "Password");
+
+    try {
+      Thread.sleep(100);
+    } catch (Exception e) {
+      e.printStackTrace();
+
+      Assert.fail(e.getMessage());
+    }
+
+    _testContext.closeImmediate();
+    _testContext.start();
+
+    UserSync user = _userManagerRef.lookup("/" + id).as(UserSync.class);
+
+    UserDataPublic userData = user.getUserData();
+
+    Assert.assertEquals(id, userData.getId());
+    Assert.assertEquals("Spock", userData.getName());
+  }
+}

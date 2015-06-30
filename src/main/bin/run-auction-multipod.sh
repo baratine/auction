53d1be3c7e7fba76084501b3e8c0e51c9e74c496
alex <rojkov@gmail.com>
2015-06-30 09:55:40 -0700

    aution mulitopd script

---
+++ src/main/bin/run-auction-multipod.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+base=`pwd`
+
+if [ -z $BARATINE_HOME ]; then
+  BARATINE_HOME=~/baratine
+fi;
+
+if [ ! -f $BARATINE_HOME/lib/baratine.jar ]; then
+  echo "BARATINE_HOME '$BARATINE_HOME' does not point to a baratine installation";
+  exit 1;
+fi;
+
+BARATINE_DATA_DIR=/tmp/baratine
+BARATINE_CONF=src/main/bin/conf-multi.cf
+BARATINE_ARGS="--data-dir $BARATINE_DATA_DIR --conf $BARATINE_CONF"
+
+$BARATINE_HOME/bin/baratine shutdown $BARATINE_ARGS
+
+rm -rf $BARATINE_DATA_DIR
+
+mvn -Dmaven.test.skip=true -P local clean package
+cp  target/auction-*.bar auction.bar
+
+mvn dependency:copy -Dartifact=com.caucho:lucene-plugin-service:1.0-SNAPSHOT:bar -Dmdep.stripVersion=true -o -DoutputDirectory=$base
+
+$BARATINE_HOME/bin/baratine start $BARATINE_ARGS --server lucene --deploy lucene-plugin-service.bar
+$BARATINE_HOME/bin/baratine start $BARATINE_ARGS --server auction --deploy auction.bar
+$BARATINE_HOME/bin/baratine start $BARATINE_ARGS --server user --deploy auction.bar
+$BARATINE_HOME/bin/baratine start $BARATINE_ARGS --server web --deploy auction.bar
+
+#echo "Create User ..."
+#$BARATINE_HOME/bin/baratine jamp-query $BARATINE_ARGS --pod web /auction-session/foo createUser user pass
+
+#echo "Authenticate User ..."
+#$BARATINE_HOME/bin/baratine jamp-query $BARATINE_ARGS --pod web /auction-session/foo login user pass
+
+#$BARATINE_HOME/bin/baratine cat $BARATINE_ARGS /proc/services
+
